3146f36fc978a963553bebb6e3c20d9886896fa4
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-04-03 09:54:40 +0200

    Started some code
    Still nothing works.....Work in progress

--- Bounce Original/Assets/Scripts/BallController.cs
+++ Bounce Original/Assets/Scripts/BallController.cs
@@ -2,7 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class BallController : MonoBehaviour {
 
-	
-}
+    float speed = 5f;
+
+    float jumpForce = 500f;
+
+    Rigidbody2D playerRB;
+
+    void Awake ()
+    {
+        playerRB = GetComponent <Rigidbody2D> ();
+    }
+
+    void FixedUpdate ()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+
+        if (Input.GetButtonDown ("Jump"))
+        {
+            playerRB.AddForce(Vector2.up * jumpForce);
+        }
+    }
+ }
