263d77453b1b76035b44abea0edb1a7359ebc96c
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-04-24 09:16:14 +0200

    Player Walks and Jumps
    Walking and jumping code successfully works

--- Bounce Original/Assets/Scripts/BallController.cs
+++ Bounce Original/Assets/Scripts/BallController.cs
@@ -10,19 +10,53 @@ public class BallController : MonoBehaviour {
 
     Rigidbody2D playerRB;
 
+    SpriteRenderer spriteRenderer;
+
+    Vector3 defaultPos;
+
     void Awake ()
     {
         playerRB = GetComponent <Rigidbody2D> ();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        defaultPos = transform.position;
     }
 
     void FixedUpdate ()
     {
         float h = Input.GetAxisRaw("Horizontal");
 
+        Move(h);
 
         if (Input.GetButtonDown ("Jump"))
         {
             playerRB.AddForce(Vector2.up * jumpForce);
         }
     }
- }
+
+    void OnBecameInvisible()
+    {
+        transform.position = defaultPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Move(float h)
+    {
+
+        //refer to the current physics movement
+        Vector2 movement = playerRB.velocity;
+        movement.x = h * speed;
+
+        // make the charcater move
+        playerRB.velocity = movement;
+
+        // check that the player is looking in the direction he's moving
+        if ((h < 0f && !spriteRenderer.flipX) || (h > 0f && spriteRenderer.flipX))
+        {
+
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+
+        }
+
+     }
+}
