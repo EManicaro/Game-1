b67739f304d0f3faec1a7feeaf588e0d1aeb85db
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-04-28 23:00:35 +0200

    Almost ready
    Main mechanics all ready, platform almost ready just few arrangments,
    scenes are not working

--- Bounce Original/Assets/Scripts/BallController.cs
+++ Bounce Original/Assets/Scripts/BallController.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BallController : MonoBehaviour {
 
+    // the character's speed
     float speed = 5f;
 
+    // the character's jump force
     float jumpForce = 250f;
 
+    // this is the character's physiscs component
     Rigidbody2D playerRB;
 
+    // this is the character's sprite renderer
     SpriteRenderer spriteRenderer;
 
+    // player lives
+    public int playerLives = 2;
+
+    // the canvas text box
+    public Text lifeText;
+
+    // the player's default position
     Vector3 defaultPos;
 
+    // when the script is activated, get its components
     void Awake ()
     {
         playerRB = GetComponent <Rigidbody2D> ();
@@ -22,20 +36,46 @@ public class BallController : MonoBehaviour {
         defaultPos = transform.position;
     }
 
+    // since we're using physics, use the FixedUpdate method
     void FixedUpdate ()
     {
+        // this tackles the left and right arrow keys
         float h = Input.GetAxisRaw("Horizontal");
 
+        // jump to the Move method
         Move(h);
 
+        // make the character jump!
         if (Input.GetButtonDown ("Jump"))
         {
             playerRB.AddForce(Vector2.up * jumpForce);
         }
     }
 
+    // when the player disappears, resst him
     void OnBecameInvisible()
     {
+        // when lives are greater then 0 minus 1, less then 0 gameover
+        if (transform.position.y < 0)
+        {
+            //if the lives are greater than 0, decrease by 1
+            if (playerLives > 1)
+            {
+                playerLives--;
+
+                //adjust the life on canvas
+                lifeText.text = "Lives: " + playerLives;
+
+                // show us the score in the console (Ctrl + Shift + C in Unity
+                Debug.Log(playerLives);
+            }
+
+            //else gameover
+            else
+            {
+                SceneManager.LoadScene("GameOver Scene");
+            }
+        }
         transform.position = defaultPos;
         transform.rotation = Quaternion.identity;
     }
--- Bounce Original/Assets/Scripts/ColliderController.cs
+++ Bounce Original/Assets/Scripts/ColliderController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColliderController : MonoBehaviour {
 
@@ -17,6 +18,11 @@ public class ColliderController : MonoBehaviour {
         {
             transform.position = defaultPos;
             transform.rotation = Quaternion.identity;
+        }
+
+        if (c.tag == "Star")
+        {
+            SceneManager.LoadScene("YouWin Scene");
         }
     }
 }
